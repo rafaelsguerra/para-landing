61446febe8cdde96af0cba5789349f976534f3c9
Rafael Guerra <rafaelsguerra@hotmail.com>
2018-12-18 16:01:36 -0300

    oloco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
